9ebf05b8aad50b40fd06b523bdbd41ffa8494584
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-05-31 14:10:05 -0700

    Add dismiss keyboard functionality

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
     @IBAction func fahrenheitFieldEditingChanges(textField: UITextField) {
         if let text = textField.text where !text.isEmpty {
             celsiusLabel.text = text
         } else {
             celsiusLabel.text = "???"
         }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject){
+        textField.resignFirstResponder()
     }
 }
