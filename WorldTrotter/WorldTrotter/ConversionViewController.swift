60f7f6d4daddbda276dd2f16fb2922afa88cbd86
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-05-31 13:19:45 -0700

    Link F text to C text

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Sydney Rossman-Reich on 5/31/16.
+//  Copyright © 2016 Sydney Rossman-Reich. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBAction func fahrenheitFieldEditingChanges(textField: UITextField) {
+        if let text = textField.text where !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+}
