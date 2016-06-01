eee6523d8efcb0f9ff43e11686b8c03246c80fb6
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-05-31 18:30:34 -0700

    Add F to C conversion

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    
     @IBOutlet var celsiusLabel: UILabel!
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
     @IBOutlet var textField: UITextField!
     
     @IBAction func fahrenheitFieldEditingChanges(textField: UITextField) {
-        if let text = textField.text where !text.isEmpty {
-            celsiusLabel.text = text
+        if let text = textField.text, value = Double(text){
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    func updateCelsiusLabel(){
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
         } else {
             celsiusLabel.text = "???"
         }
